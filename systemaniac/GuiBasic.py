aa0bc98e2d1ea361e6793b9a0074336c1af8ca5c
Hridoy-31 <raihanulbashirhridoy@gmail.com>
2022-06-11 00:37:40 +0600

    Error catching of PyGObject

--- systemaniac/GuiBasic.py
+++ systemaniac/GuiBasic.py
@@ -5,3 +5,13 @@ Basic GUI code
 from systemaniac import _
 
 import os
+
+try:
+    import gi
+except ModuleNotFoundError as e:
+    print('#'*60)
+    print("Please install PyGObject")
+    print("https://pygobject.readthedocs.io/en/latest/getting_started.html")
+    print('#'*60)
+    raise e
+
